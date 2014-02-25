4bf4eed66930323ae939e7eed05f275a72555815
Tim <tim35050@gmail.com>
2014-02-24 21:06:40 -0800

    HW5

--- code/gini.py
+++ code/gini.py
@@ -5,6 +5,8 @@ Classes are strings."""
 
 import fileinput
 import csv
+from collections import defaultdict
+from collections import Counter 
 
 (
     CMTE_ID, AMNDT_IND, RPT_TP, TRANSACTION_PGI, IMAGE_NUM, TRANSACTION_TP,
@@ -13,13 +15,18 @@ import csv
     MEMO_CD, MEMO_TEXT, SUB_ID
 ) = range(22)
 
+OBAMA_ID = 'P80003338'
+ROMNEY_ID = 'P80003353'
+
 CANDIDATES = {
-    'P80003338': 'Obama',
-    'P80003353': 'Romney',
+    OBAMA_ID: 'Obama',
+    ROMNEY_ID: 'Romney',
 }
 
 ############### Set up variables
-# TODO: declare datastructures
+all_contributed_candidate_ids = []
+zip_contributed_candidate_ids = defaultdict(list)
+zip_count = defaultdict(int)
 
 ############### Read through files
 for row in csv.reader(fileinput.input(), delimiter='|'):
@@ -28,15 +35,42 @@ for row in csv.reader(fileinput.input(), delimiter='|'):
         continue
 
     candidate_name = CANDIDATES[candidate_id]
+    all_contributed_candidate_ids.append(candidate_id)
     zip_code = row[ZIP_CODE]
-    ###
-    # TODO: save information to calculate Gini Index
-    ##/
+    zip_contributed_candidate_ids[zip_code].append(candidate_id)
+    zip_count[zip_code] += 1
+
+def get_overall_gini_index(contributed_candidate_ids):
+    candidate_counter = Counter()
+    candidate_counter.update(contributed_candidate_ids)
+    candidate_counts = list(candidate_counter.values())
+    return calculate_gini(candidate_counts)
+
+def get_weighted_gini_index(zip_contributed_candidate_ids, zip_count):
+    zip_candidate_counts = {}
+    total_count = sum(list(zip_count.values()))
+    weighed_ginis = []
+    for zip_code in zip_contributed_candidate_ids:
+        candidate_counter = Counter()
+        candidate_counter.update(zip_contributed_candidate_ids[zip_code])
+        candidate_counts = list(candidate_counter.values())
+        gini = calculate_gini(candidate_counts)
+        fraction = zip_count[zip_code] / float(total_count)
+        weighed_ginis.append(gini * fraction)
+    return sum(weighed_ginis)
+
+def calculate_gini(counts):
+    gini_sum = 0
+    squared_fracs = []
+    total = sum(counts)
+    for count in counts:
+        squared_fracs.append((count/float(total))**2)
+    return 1 - sum(squared_fracs)
 
 ###
 # TODO: calculate the values below:
-gini = 0  # current Gini Index using candidate name as the class
-split_gini = 0  # weighted average of the Gini Indexes using candidate names, split up by zip code
+gini = get_overall_gini_index(all_contributed_candidate_ids)  # current Gini Index using candidate name as the class
+split_gini = get_weighted_gini_index(zip_contributed_candidate_ids, zip_count)  # weighted average of the Gini Indexes using candidate names, split up by zip code
 ##/
 
 print "Gini Index: %s" % gini
