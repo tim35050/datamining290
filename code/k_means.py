a88001ffbdf5b7217bdc3b1acff77acf6382c0e5
Tim <tim35050@gmail.com>
2014-03-12 22:21:54 -0700

    HW7-kmeans

--- code/k_means.py
+++ code/k_means.py
@@ -3,6 +3,8 @@
 #
 ##/
 
+import random
+
 dataset = [-13.65089255716321, -0.5409562932238607, -88.4726466247223, 39.30158828358612, 4.066458182574449, 64.64143300482378, 38.68269424751338, 33.42013676314311, 31.18603331719732, -0.2027616409406292, 45.13590038987272, 30.791899783552395, 61.1727490302448, 18.167220741624856, 88.88077709786394, -1.3808002119514704, 50.14991362212521, 55.92029956281276, -6.759813255299466, 34.28290084421072]
 k = 2 # number of clusters
 
@@ -14,22 +16,25 @@ k = 2 # number of clusters
 def pick_centroids(xs, num):
     """Return list of num centroids given a list of numbers in xs"""
     ###
-    # TODO select and return centroids
-    return [1,2]
+    # Randomly pick num centroids from  uniform distribution formed from min and max of dataset
+    centroids = []
+    for i in range(num):
+        centroids.append(random.choice(dataset))
+    return centroids
     ##/
 
 def distance(a, b):
     """Return the distance of numbers a and b"""
     ###
-    # TODO return correct expression
-    return 0
+    # distance defined as absolute difference
+    return abs(a - b)
     ##/
 
 def centroid(xs):
     """Return the centroid number given a list of numbers, xs"""
     ###
-    # TODO calculate and return centroid
-    return 0
+    # Calculate centroid as mean of numbers in list
+    return sum(xs) / float(len(xs))
     ##/
 
 def cluster(xs, centroids):
